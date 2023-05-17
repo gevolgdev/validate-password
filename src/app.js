ddeaa2f4adfc3ecae475cd748b4460d5cf56ca80
Pinelli <ricardosilvapinelli@gmail.com>
2023-05-17 16:04:40 -0300

    Validaçao e feedback

--- src/app.js
+++ src/app.js
@@ -3,19 +3,32 @@ const buttonPassword = document.getElementById('validate-button');
 const passContainer = document.querySelector('.pass');
 // ----------------------------------------------------------------
 
+const renderFeedback = (message) => {
+  passContainer.innerHTML = '';
+  const passElement = document.createElement('div');
+  passElement.innerHTML = `<span class='feedback'>${message}</span>`;
+  passContainer.appendChild(passElement);
+  setTimeout(() => {
+    passElement.innerHTML = '';
+    return;
+  }, 5000);
+};
+
 const userPass = () => inputPassword.value;
 
 const verifyPassword = (password) => {
   if(password.length < 8 || password.length > 20) {
-    console.log('Muito curto ou muito longa.');
+    password.length < 8 && renderFeedback('Senha curta!');
+    password.length > 20 && renderFeedback('Senha muito longa!');
     return;
   };
   if(!/[a-z]/.test(password) || !/[A-Z]/.test(password)) {
-    console.log('Sem letra minuscula ou maiuscula.');
+    !/[a-z]/.test(password) || renderFeedback('Coloque um letra minuscula.');
+    !/[A-Z]/.test(password)|| renderFeedback('Coloque um letra maiúscula.');
     return;
   };
   if(!/[0-9]/.test(password)) {
-    console.log('Sem numero.');
+    renderFeedback('Coloque um número.');
     return;
   };
   const specialCaracter = "!@#$%^&*()_-+=|]}[{':;?/>.<,~".split('');
@@ -27,14 +40,15 @@ const verifyPassword = (password) => {
     };
   };
   if(noSpecialCaracter) {
-    console.log('Sem caracteres especial.');
+    renderFeedback('Coloque um caráctere especial.');
     return;
   };
 
-  console.log('Senha protegida!');
+  renderFeedback('Senha protegida!');
 };
 
 const handleValidate = () => {
   verifyPassword(userPass());
+  inputPassword.value = '';
 };
 buttonPassword.addEventListener('click', handleValidate);
